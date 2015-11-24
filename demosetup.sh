d936f55c00ce0d6a4786f9fea046c96d8e301f64
Nirav Mehta <mehtanirav@live.com>
2015-11-23 20:28:07 -0500

    Update demosetup.sh
    Changed to use the tutum/haproxy container instead of a static one.

--- demosetup.sh
+++ demosetup.sh
@@ -9,14 +9,17 @@ eval "$(docker-machine env demodev)"
 docker create -v /data/db --name dbdata debian:wheezy /bin/true
 docker run -d --name enrolldb --volumes-from dbdata mongo:2.4.14
 
-# Setup the backend app
-docker run -d --link enrolldb:mongo --name app namehta/enrollment-app:tomcat
+# Setup 3 instances of the backend app
+docker run -d --link enrolldb:mongo --name app-1 -e VIRTUAL_HOST="*/app/*" namehta/enrollment-app:tomcat
+docker run -d --link enrolldb:mongo --name app-2 -e VIRTUAL_HOST="*/app/*" namehta/enrollment-app:tomcat
+docker run -d --link enrolldb:mongo --name app-3 -e VIRTUAL_HOST="*/app/*" namehta/enrollment-app:tomcat
 
-# Setup the frontend app
-docker run -d --name ui namehta/enrollment-ui
+# Setup 2 instances of the frontend app
+docker run -d --name ui-1 -e VIRTUAL_HOST="demoapp.example.com" namehta/enrollment-ui
+docker run -d --name ui-2 -e VIRTUAL_HOST="demoapp.example.com" namehta/enrollment-ui
 
 # Setup the proxy
-docker run -d -p 80:80 --link ui:ui --link app:app --name enrollment-proxy namehta/enrollment-proxy
+docker run -d -p 80:80 --link app-1:app-1 --link app-2:app-2 --link app-3:app-3 --link ui-1:ui-1 --link ui-2:ui-2 tutum/haproxy
 
 # Add an entry for demoapp.example.com to /etc/hosts
 if [ -n "$(grep 'demoapp.example.com' /etc/hosts)" ]
